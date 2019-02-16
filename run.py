3831dc2fd83aec5f5ee13a52cf757e59b25919b3
mas9311 <mas9311@gmail.com>
2019-02-15 17:55:57 -0700

    Added a fallback in case the user enters an unknown argument.

--- run.py
+++ run.py
@@ -5,16 +5,19 @@ import time
 from sample import parameters, Tree
 
 if __name__ == '__main__':
-    # parser = parameters.retrieve()
-    my_tree = Tree.Tree(parameters.retrieve())
-    # print(f'width    = {str(parser.parse_args().width)}\n'
-    #       f'speed    = {str(parser.parse_args().speed)}\n'
-    #       f'density  = {str(parser.parse_args().density)}\n'
-    #       f'tiers    = {str(parser.parse_args().tiers)}\n'
-    #       f'ornaments= {str(parser.parse_args().yes)}')
+    args_to_parse = parameters.retrieve()
+    unknown_args = args_to_parse.parse_known_args()[1]
+    if unknown_args:
+        args_to_parse.print_help()
+        print('\nThe argument(s)', unknown_args, 'are not valid.')
+        input('\nPress [Enter] to quit\n>')
+        args_to_parse.parse_args()
+        raise Exception('Unknown arguments')  # Redundant Exception to ensure failure
+    my_tree = Tree.Tree(args_to_parse.parse_args())
     count = 0
     while True:
         print(my_tree)
-        if count > 6:
-            time.sleep(my_tree.sleep_time)
-        count += 1
+        if count <= 5:
+            count += 1
+            continue
+        time.sleep(my_tree.sleep_time)
