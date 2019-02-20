39d73ed04d8b834bf09b11243699e3aa5cdfc32a
mas9311 <mas9311@gmail.com>
2019-02-20 15:30:06 -0700

    Added comments so the user can modify the length of the tree_list

--- run.py
+++ run.py
@@ -5,15 +5,22 @@ import time
 from sample import parameters, Tree
 
 if __name__ == '__main__':
+    # create a template for the Tree
     my_tree = Tree.Tree(parameters.retrieve())
+
+    # max length of list to compute
     max_len = 10
+
+    # creates a list of 10 unique snow/ornament arrangements
     tree_list = my_tree.build_list(max_len)
 
+    # print the first 6 upon execution (to fill the screen)
     for curr_tree in range(6):
         print(tree_list[curr_tree])
 
+    # continuous loop that iterates through the list of trees
     while True:
         for curr_tree in range(max_len):
             print(tree_list[curr_tree])
-            print(curr_tree)
+            # pause execution for the time specified in the --speed argument
             time.sleep(my_tree.sleep_time)
