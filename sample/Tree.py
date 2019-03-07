814230986c23100063f36f4a628fe8b93e0c785f
mas9311 <mas9311@gmail.com>
2019-03-06 22:56:25 -0700

    Added docstring to Tree's build_list method and underscores on the methods that a user won't use

--- sample/Tree.py
+++ sample/Tree.py
@@ -32,55 +32,55 @@ class Tree:
         self.snow_char = '*'  # * ❇
         self.base_char = '┆'  # │ ║ ┃ ┆ ┇ ┊ ┋
 
-    def gen_snow(self, snow_len):
+    def _gen_snow(self, snow_len):
         output = ''
         for _ in range(snow_len):
             rand_snow = int(np.random.random_sample() * 1000)
             output += (' ', self.snow_char)[0 <= rand_snow <= 0 + self.max_snow]
         return output
 
-    def tree_topper(self):
-        output = self.star_line()
+    def _tree_topper(self):
+        output = self._star_line()
         for leaves_width in range(1, 10, 2):  # 1, 3, 5, 7, 9
-            output += self.tree_line(leaves_width)
-        return self.gen_snow(self.screen_width) + '\n' + output + self.tree_line(13)
+            output += self._tree_line(leaves_width)
+        return self._gen_snow(self.screen_width) + '\n' + output + self._tree_line(13)
 
-    def tree_shape(self):
+    def _tree_shape(self):
         output = ''
         neck = 7
         for _ in range(self.tree_tiers - 1):
             for curr in range(4):
                 w = neck + (curr * 4)
                 if curr is 0:
-                    output += self.tree_line(w, 8)
+                    output += self._tree_line(w, 8)
                 elif curr is 1:
-                    output += self.tree_line(w, 4)
+                    output += self._tree_line(w, 4)
                 else:
-                    output += self.tree_line(w)
+                    output += self._tree_line(w)
             neck += 4
         return output
 
-    def tree_base(self):
+    def _tree_base(self):
         output = ''
         base_width = 1 + ((self.tree_tiers // 2) * 2)
         base_width = (base_width, 9)[base_width > 9]
         base_height = (self.tree_tiers - 1, 1)[self.tree_tiers is 1]
         base_height = (base_height, 6)[base_height > 6]
         for _ in range(base_height):
-            output += self.make_base(base_width)
-        return output + self.gen_snow(self.screen_width)
+            output += self._make_base(base_width)
+        return output + self._gen_snow(self.screen_width)
 
-    def star_line(self):
+    def _star_line(self):
         spaces = ((self.screen_width - 3) // 2)
-        return self.gen_snow(spaces) + ' ' + self.star_char + ' ' + self.gen_snow(spaces + self.make_even) + '\n'
+        return self._gen_snow(spaces) + ' ' + self.star_char + ' ' + self._gen_snow(spaces + self.make_even) + '\n'
 
-    def tree_line(self, leaves, buffer_len=2):
+    def _tree_line(self, leaves, buffer_len=2):
         buffer = ' ' * (buffer_len // 2)
-        leaves_str = self.gen_leaf(leaves)
+        leaves_str = self._gen_leaf(leaves)
         spaces = (self.screen_width - (buffer_len + leaves)) // 2
-        return self.gen_snow(spaces) + buffer + leaves_str + buffer + self.gen_snow(spaces + self.make_even) + '\n'
+        return self._gen_snow(spaces) + buffer + leaves_str + buffer + self._gen_snow(spaces + self.make_even) + '\n'
 
-    def gen_leaf(self, leaves):
+    def _gen_leaf(self, leaves):
         output = ''
         for _ in range(leaves):
             r = int(np.random.random_sample() * 1000)
@@ -98,17 +98,18 @@ class Tree:
                 output += self.leaf_char
         return output
 
-    def make_base(self, width):
+    def _make_base(self, width):
         buffer = ' ' * ((self.tree_width - width - 2) // 2)
         base_str = self.base_char * width
         spaces = (self.screen_width - self.tree_width + 2) // 2
-        return self.gen_snow(spaces) + buffer + base_str + buffer + self.gen_snow(spaces + self.make_even) + '\n'
+        return self._gen_snow(spaces) + buffer + base_str + buffer + self._gen_snow(spaces + self.make_even) + '\n'
 
     def build_list(self, max_len=25):
+        """Returns a list of generated snow and ornament arrangements to be used to print."""
         tree_list = [str(self) for _ in range(max_len)]
         return tree_list
 
     def __str__(self):
-        return (f'{self.tree_topper()}'
-                f'{self.tree_shape()}'
-                f'{self.tree_base()}')
+        return (f'{self._tree_topper()}'
+                f'{self._tree_shape()}'
+                f'{self._tree_base()}')
