6df63a099a21f53f2a180d45b7b6043323a5eb9f
mas9311 <mas9311@gmail.com>
2019-03-08 02:38:11 -0700

    Correctly displays the Tree in the gui

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+##### -*- coding: utf-8 -*-
 
 import time
-from sample import parameters, Tree
+from sample import interface, parameters, Tree
 
 if __name__ == '__main__':
     """The controller of the execution. Retrieves the parsed arguments, and if they're valid, create a list of
@@ -18,13 +18,14 @@ if __name__ == '__main__':
     # creates a list of 25 unique snow/ornament arrangements
     tree_list = my_tree.build_list(max_len)
 
-    # print the first 6 upon execution to immediately fill the screen with snowy trees
-    for curr_tree in range(6):
-        print(tree_list[curr_tree])
-
-    # continuous loop that iterates through the list of trees
-    while True:
-        for curr_tree in range(max_len):
-            print(tree_list[curr_tree])
-            # pause execution for the time specified in the --speed argument provided.
-            time.sleep(my_tree.sleep_time)
+    interface.gui(my_tree, tree_list, max_len)
+    # # print the first 6 upon execution to immediately fill the screen with snowy trees
+    # for curr_tree in range(6):
+    #     print(tree_list[curr_tree])
+    #
+    # # continuous loop that iterates through the list of trees
+    # while True:
+    #     for curr_tree in range(max_len):
+    #         print(tree_list[curr_tree])
+    #         # pause execution for the time specified in the --speed argument provided.
+    #         time.sleep(my_tree.sleep_time)
--- sample/interface.py
+++ sample/interface.py
@@ -1,32 +1,39 @@
 import tkinter as tk
+import tkinter.font as tkf
 import time
 
 
-def run_gui(my_tree, tree_list, max_len):
+def run_gui(textbox, my_tree, tree_list, index, max_len):
+    # continuous loop that iterates through the list of trees
+    # while True:
+    #     for curr_tree in range(max_len):
+    textbox.insert('0.0', tree_list[index] + '\n')
+    # pause execution for the time specified in the --speed argument provided.
+    textbox.after(int(my_tree.sleep_time * 1000), run_gui, textbox, my_tree, tree_list, (index + 1) % max_len, max_len)
+
+
+def gui(my_tree, tree_list, max_len):
     root = tk.Tk()
     root.title('')
     # root.overrideredirect(1)
     root.configure(borderwidth='0')
-    # root.geometry('2552x1381')
-    root.geometry('400x100+200+200')
+    root.geometry('820x820+0+0')
+    scrollbar = tk.Scrollbar(root)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
     close_button = tk.Button(root, text='X', command=root.destroy)
-    close_button.pack(side=tk.RIGHT, fill=tk.BOTH)
+    close_button.pack(side=tk.RIGHT, fill=tk.Y)
 
-    textbox = tk.Text(root, fg='green', background='#000000', font=12)
-    textbox.pack()
-    quote = """**********************"""
-    textbox.insert(tk.END, quote)
-
+    text_font = tkf.Font(family='DejaVu Sans fixed', slant='roman', size=-12)
+    textbox = tk.Text(root, fg='green', background='#000000', height=1000, width=50, wrap='none', font=text_font)
+    textbox.pack(fill=tk.BOTH)
+    textbox.config(yscrollcommand=scrollbar.set)
+    print(tkf.families())
     # print the first 6 upon execution to immediately fill the screen with snowy trees
+    initial_tree_str = ''
     for curr_tree in range(6):
-        # print(tree_list[curr_tree])
-        textbox.insert(tk.END, quote)
+        initial_tree_str += tree_list[curr_tree] + '\n'
+    textbox.insert(tk.END, initial_tree_str)
 
-    # continuous loop that iterates through the list of trees
-    while True:
-        for curr_tree in range(max_len):
-            print(tree_list[curr_tree])
-            # pause execution for the time specified in the --speed argument provided.
-            time.sleep(my_tree.sleep_time)
-
+    run_gui(textbox, my_tree, tree_list, 6, max_len)
     root.mainloop()
