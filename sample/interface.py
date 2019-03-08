de3f1dce100d05e974d928260d5e01e8e1213330
mas9311 <mas9311@gmail.com>
2019-03-08 05:10:02 -0700

    Add gui interface class and. This class inheritted the old __main__ functionality. Prints the Snowy-Tree to the GUI, but I need to fix the NorthEast 'close' Button

--- sample/interface.py
+++ sample/interface.py
@@ -1,39 +1,56 @@
-import tkinter as tk
-import tkinter.font as tkf
-import time
+from tkinter import *
+
+from sample import Tree, parameters
 
 
-def run_gui(textbox, my_tree, tree_list, index, max_len):
-    # continuous loop that iterates through the list of trees
-    # while True:
-    #     for curr_tree in range(max_len):
-    textbox.insert('0.0', tree_list[index] + '\n')
-    # pause execution for the time specified in the --speed argument provided.
-    textbox.after(int(my_tree.sleep_time * 1000), run_gui, textbox, my_tree, tree_list, (index + 1) % max_len, max_len)
+class TreeGUI:
+    def __init__(self, root):
+        # creates the template of the Tree to print; snow & ornaments are unique upon printing.
+        self.tree = Tree.Tree(parameters.retrieve())
 
+        # max length of list to compute. This will save you energy from each snowflake's numpy.random call.
+        self.max_len = 25
 
-def gui(my_tree, tree_list, max_len):
-    root = tk.Tk()
-    root.title('')
-    # root.overrideredirect(1)
-    root.configure(borderwidth='0')
-    root.geometry('820x820+0+0')
-    scrollbar = tk.Scrollbar(root)
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        # creates a list of 25 unique snow/ornament arrangements
+        self.tree_list = self.tree.build_list(self.max_len)
 
-    close_button = tk.Button(root, text='X', command=root.destroy)
-    close_button.pack(side=tk.RIGHT, fill=tk.Y)
+        self.root = root
+        self.set_root()
 
-    text_font = tkf.Font(family='DejaVu Sans fixed', slant='roman', size=-12)
-    textbox = tk.Text(root, fg='green', background='#000000', height=1000, width=50, wrap='none', font=text_font)
-    textbox.pack(fill=tk.BOTH)
-    textbox.config(yscrollcommand=scrollbar.set)
-    print(tkf.families())
-    # print the first 6 upon execution to immediately fill the screen with snowy trees
-    initial_tree_str = ''
-    for curr_tree in range(6):
-        initial_tree_str += tree_list[curr_tree] + '\n'
-    textbox.insert(tk.END, initial_tree_str)
+        self.frame = Frame(root)
+        self.frame.pack(fill=BOTH)
 
-    run_gui(textbox, my_tree, tree_list, 6, max_len)
-    root.mainloop()
+        self.close_button = Button(self.frame, text='X', command=root.destroy)
+        self.close_button.configure(width=1, height=1)
+        # TODO: allow button placement "floating" or overlaying the frame
+        self.close_button.place(x=150, y=150)
+        self.close_button.pack(anchor=NE)
+
+        self.textbox = Text(self.frame, fg='green', background='#000000', height=1000, width=50, wrap='none', font='fixed')
+        self.textbox.pack(fill=BOTH)
+
+        # print the first 6 upon execution to immediately fill the screen with snowy trees
+        self.print_init()
+
+        # continue execution
+        self.run_gui(6)
+        self.root.update()
+
+    def set_root(self):
+        self.root.overrideredirect(1)
+        self.root.configure(borderwidth='0')
+        self.root.geometry('1074x1100+0+0')
+
+    def print_init(self):
+        initial_tree_str = ''
+        for curr_tree in range(6):
+            initial_tree_str += self.tree_list[curr_tree] + '\n'
+        self.textbox.insert(END, initial_tree_str)
+
+    def run_gui(self, index):
+        """Recursive loop that prints the tree at the top of the GUI"""
+        self.textbox.insert('0.0', self.tree_list[index] + '\n')
+
+        # pause execution for the time specified in the --speed argument provided.
+        self.textbox.after(int(self.tree.sleep_time * 1000),
+                           self.run_gui, (index + 1) % self.max_len)
