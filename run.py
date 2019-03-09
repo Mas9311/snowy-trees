a2b1a5cd07ae9e4ac4d1d80655d37c8d40432ce2
mas9311 <mas9311@gmail.com>
2019-03-08 22:36:04 -0700

    Rename the tree_gui to app, because I plan on making this program cli and gui accessible

--- run.py
+++ run.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     """The controller of the execution. Retrieves the parsed arguments, and if they're valid, create a list of
     25 Trees (each with their own unique snow/ornament arrangements), then loop the same 25 forever."""
     root = Tk()
-    tree_gui = interface.TreeGUI(root)
+    app = interface.TreeGUI(root)
     root.mainloop()
