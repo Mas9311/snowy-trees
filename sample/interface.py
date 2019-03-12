5f5cc8c5332615474ae573e0526ec487b7366052
mas9311 <mas9311@gmail.com>
2019-03-12 04:09:07 -0600

    Added ornaments Buttons (nested in an ornaments Frame) and set the default value according to the initial argparsed values

--- sample/interface.py
+++ sample/interface.py
@@ -19,7 +19,7 @@ class TreeGUI(Frame):
         self.window_close = None
         self.window_minimize = None
         self.window_maximize = None
-        self.maximized = False
+        self.maximized_bool = False
         self.set_win_man_frame()
 
         self.opt_frame = None
@@ -45,6 +45,12 @@ class TreeGUI(Frame):
         self.opt_tiers = None
         self.int_tiers = None
 
+        self.opt_ornaments_label = None
+        self.ornaments_bool = None
+        self.opt_ornaments_frame = None
+        self.opt_ornaments_on = None
+        self.opt_ornaments_off = None
+
         self.init_options()
 
         # self.update()
@@ -53,7 +59,6 @@ class TreeGUI(Frame):
         self.h_dim = 117 * self.tree.tree_tiers + 163  # self.winfo_height() * 2
         self.x_dim = self.winfo_x()
         self.y_dim = self.winfo_y()
-        print(self.w_dim, self.h_dim, self.x_dim, self.y_dim)
 
         self.set_root()
 
@@ -110,19 +115,19 @@ class TreeGUI(Frame):
 
     def _close(self):
         self.root.destroy()
-        # self.root.state('withdrawn')
 
     def _maximize(self):
-        if not self.maximized:
-            self.maximized = True
+        if not self.maximized_bool:
             self.root.overrideredirect(1)
             self.root.call("wm", "attributes", ".", "-fullscreen", "true")
         else:
-            self.maximized = False
             self.root.overrideredirect(0)
             self.root.call("wm", "attributes", ".", "-fullscreen", "false")
+        self.maximized_bool = not self.maximized_bool
 
     def _minimize(self):
+        if self.maximized_bool:
+            self._maximize()
         self.root.state('iconic')
 
     def set_opt_button(self):
@@ -139,12 +144,10 @@ class TreeGUI(Frame):
         self.set_opt_speed()
         self.set_opt_density()
         self.set_opt_tiers()
+        self.set_opt_ornaments()
 
-        d = Button(self.opt_frame, text='4th', font=('courier', 25), bg='gold', highlightthickness=0, command=self._d)
-        d.grid(row=7, column=0, sticky=W + E)
-
-    def _d(self):
-        print('*' * 35)
+        d = Button(self.opt_frame, text='4th', font=('courier', 25), bg='gold', highlightthickness=0)
+        d.grid(row=9, column=0, sticky=W + E)
 
     def set_opt_speed(self):
         self.opt_speed_label = Label(self.opt_frame, text='Refresh Speed', bg='#aaaaaa', fg='#3d008e',
@@ -179,6 +182,35 @@ class TreeGUI(Frame):
         self.opt_tiers.set(self.int_tiers)
         self.opt_tiers.grid(row=6, column=0, sticky=W + E)
 
+    def set_opt_ornaments(self):
+        self.opt_ornaments_label = Label(self.opt_frame, text='Ornaments', bg='#333333', fg='#00d165',
+                                         highlightthickness=0, font=('courier', 25), relief=FLAT)
+        self.opt_ornaments_label.grid(row=7, column=0, sticky=W + E)
+
+        self.ornaments_bool = (False, True)[self.tree.arg_dict['ornaments']]
+
+        self.set_opt_ornaments_buttons()
+
+    def set_opt_ornaments_buttons(self):
+        if self.opt_ornaments_frame:
+            self.opt_ornaments_frame.grid_forget()
+        self.opt_ornaments_frame = Frame(self.opt_frame)
+        self.opt_ornaments_frame.grid(row=8, column=0, sticky=W + E)
+
+        button_on = (FLAT, RIDGE)[self.ornaments_bool]
+        button_off = (RIDGE, FLAT)[self.ornaments_bool]
+
+        self.opt_ornaments_on = Button(self.opt_ornaments_frame, text='On', highlightthickness=0, width=6,
+                                       font=('courier', 25), relief=button_on, bg='#333333', fg='#00d165',
+                                       activebackground='#333333', activeforeground='#00d165',
+                                       command=self.set_ornaments_on)
+        self.opt_ornaments_on.pack(side=LEFT, fill=BOTH)
+        self.opt_ornaments_off = Button(self.opt_ornaments_frame, text='Off', highlightthickness=0, width=6,
+                                        font=('courier', 25), relief=button_off, bg='#333333', fg='#00d165',
+                                        activebackground='#333333', activeforeground='#00d165',
+                                        command=self.set_ornaments_off)
+        self.opt_ornaments_off.pack(side=RIGHT, fill=BOTH)
+
     def set_options(self):
         if self.opt_frame:
             self.opt_frame.destroy()
@@ -191,6 +223,7 @@ class TreeGUI(Frame):
     def set_speed(self, value):
         if not self.busy:
             self.busy = True
+            before = self.curr_speed
             value = int(value)
             args = self.tree.arg_dict
             speeds = ['slow', 'average', 'fast', 'ultra']
@@ -198,44 +231,60 @@ class TreeGUI(Frame):
             self.curr_speed = speeds[self.int_speed - 1]
             args['speed'] = self.curr_speed
             self.reset_tree(args)
-            print('speed', self.curr_speed)
+            print_change('Speed', before, self.curr_speed)
 
     def set_density(self, value):
         value = int(value)
         if self.int_density is not value and not self.busy:
             self.busy = True
+            before = self.curr_density
             args = self.tree.arg_dict
             densities = ['thin', 'average', 'heavy', 'ultra']
             self.int_density = value
             self.curr_density = densities[self.int_density - 1]
             args['density'] = self.curr_density
             self.reset_tree(args)
-            print('density', self.curr_density)
+            print_change('Density', before, self.curr_density)
 
     def set_tiers(self, value):
         if not self.busy:
             self.busy = True
+            before = self.int_tiers
             value = int(value)
             args = self.tree.arg_dict
             self.int_tiers = value
             args['tiers'] = self.int_tiers
             self.reset_tree(args)
-            print('tiers', self.int_tiers)
-            # if self.h_dim < 117 * self.tree.tree_tiers + 163:
-            #     print('should enlarge', self.h_dim, 117 * self.tree.tree_tiers + 163)
-            #     self.h_dim = 117 * self.tree.tree_tiers + 163
-            #     self.root.geometry('{}x{}+{}+{}'.format(self.w_dim, self.h_dim, self.x_dim, self.y_dim))
+            print_change('Tiers', before, self.int_tiers)
+
+    def set_ornaments_on(self):
+        self.set_ornaments(True)
+
+    def set_ornaments_off(self):
+        self.set_ornaments(False)
+
+    def set_ornaments(self, arg_bool):
+        if not self.busy and arg_bool is not self.ornaments_bool:
+            self.busy = True
+            before = self.ornaments_bool
+            args = self.tree.arg_dict
+            args['ornaments'] = arg_bool
+            self.reset_tree(args)
+            self.ornaments_bool = arg_bool
+            self.set_opt_ornaments_buttons()
+            print_change('Ornaments', before, self.ornaments_bool)
 
     def set_screen_width(self):
         if not self.busy:
             self.busy = True
+            before = self.tree.screen_width
             args = self.tree.arg_dict
             args['width'] = max(self.w_dim // 6 - 2, self.tree.tree_width + 1)
             self.reset_tree(args)
-            print('screen_width', self.tree.screen_width)
+            print_change('Window Width', before, self.tree.screen_width)
 
     def reset_tree(self, args):
-        print('reset')
+        # print('reset')
         self.textbox.place_forget()
         self.set_text_box()
 
@@ -245,6 +294,7 @@ class TreeGUI(Frame):
         self.busy = False
 
     def window_change(self, event):
+        # before = f'{self.w_dim}x{self.h_dim}+{self.x_dim}+{self.y_dim}'
         self.w_dim = self.winfo_width()
         if self.tree.screen_width is not self.w_dim // 6 - 2 and self.w_dim // 6 - 2 > self.tree.tree_width + 1:
             self.set_screen_width()
@@ -252,6 +302,7 @@ class TreeGUI(Frame):
         self.x_dim = self.winfo_rootx()
         self.y_dim = self.winfo_rooty()
         self.root.geometry('{}x{}'.format(self.w_dim, self.h_dim))
+        # print_change('Dimensions', before, f'{self.w_dim}x{self.h_dim}+{self.x_dim}+{self.y_dim}')
 
     def print_init(self):
         """Print the initial 6 Trees to the GUI"""
@@ -262,9 +313,13 @@ class TreeGUI(Frame):
 
     def run_gui(self, textbox, index):
         """Recursive loop that prints the tree at the top of the GUI"""
-        print(f'{self.w_dim}x{self.h_dim}+{self.x_dim}+{self.y_dim}')
-
         textbox.insert('0.0', self.tree.list[index] + '\n')
         # pause execution for the time specified in the speed argument provided.
         textbox.after(int(self.tree.sleep_time * 1000),
                       self.run_gui, textbox, (index + 1) % self.tree.list_len)
+
+
+def print_change(type_of, before, after):
+    if str(before) != str(after):
+        indent = (' * ', '')[type_of != 'Dimensions']
+        print(f'{indent}{type_of}: {before} => {after}')
