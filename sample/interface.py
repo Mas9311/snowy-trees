6572b53908274f34f26c215e244b4bc769c6f601
mas9311 <mas9311@gmail.com>
2019-03-14 01:34:45 -0600

    Learning has occurred. Where to even begin... Just tweaked a little of everything for efficiency. No longer .destroy()s every Frame. Speeds up responsiveness to an option being changed. Actually, everything is instantaneous. Added a Font pattern as window_manager_font and options_font. Preparing for users to change the font. Whenever **any** option is changed, fill the screen (and then one more tree) to reflect the new option. Leaving the instaneous feedback. tkinter is the absolute best! And I'm only scratching the surface. If you're reading this, then you're just as weird as i am, and I appreciate you.

--- sample/interface.py
+++ sample/interface.py
@@ -1,4 +1,5 @@
 import time
+from tkinter import font
 from tkinter import *
 
 from sample import Tree, parameters
@@ -43,6 +44,7 @@ class GUI(Frame):
         self.set_text_box()
 
         # TODO: refactor window_manager_frame into a separate class
+        self.window_manager_font = None
         self.window_manager_frame = None
         self.window_close = None
         self.window_minimize = None
@@ -51,6 +53,7 @@ class GUI(Frame):
         self.set_win_man_frame()
 
         # TODO: refactor opt_frame into a separate class
+        self.options_font = None
         self.opt_frame = None
         self.opt_bool = None
         self.opt_button = None
@@ -84,43 +87,42 @@ class GUI(Frame):
 
         # self.update()
         self.update_idletasks()
-        self.w_dim = self.tree.screen_width * 6 + 12  # self.winfo_width() - 6
-        self.h_dim = 117 * self.tree.tree_tiers + 163  # self.winfo_height() * 2
+        self.w_dim = (self.tree.screen_width + self.tree.make_even + 2) * 6
+        self.h_dim = int(self.tree.screen_height * 13 * 2)  # prints 2 trees
         self.x_dim = self.winfo_x()
         self.y_dim = self.winfo_y()
 
         self.set_root()
 
-        # print the first 6 upon execution to immediately fill the screen with snowy trees
-        self.print_init()
+        # immediately fills the current GUI window with trees
+        self.print_trees_now()
 
         # continue execution
-        self.busy = False
-        self.run_gui(self.textbox, 6)
-        # self.set_screen_width()
+        self.run_gui(self.textbox, 0)
 
     def init_options(self):
-        speeds = ['slow', 'average', 'fast', 'ultra']
+        speeds = parameters.retrieve_speed_choices()
         self.int_speed = speeds.index(self.tree.arg_dict['speed']) + 1
         self.curr_speed = speeds[self.int_speed - 1]
 
-        densities = ['thin', 'average', 'heavy', 'ultra']
+        densities = parameters.retrieve_density_choices()
         self.int_density = densities.index(self.tree.arg_dict['density']) + 1
         self.curr_density = densities[self.int_density - 1]
 
         self.int_tiers = self.tree.tree_tiers
 
+        self.ornaments_bool = self.tree.arg_dict['ornaments']
+
     def set_root(self):
         self.root.bind('<Configure>', self.window_change)
         self.root.title('Snowy Trees')
         self.root.resizable(width=True, height=True)
-        # self.root.configure(bd=0)
         self.root.geometry('{}x{}+{}+{}'.format(self.w_dim, self.h_dim, self.x_dim, self.y_dim))
 
     def set_text_box(self):
-        self.textbox = Text(self, fg='green', background='#000000', height=200, width=150,
+        self.textbox = Text(self, fg='green', background='#000000',
                             wrap='none', font='fixed', highlightthickness=0)
-        self.textbox.pack(fill=BOTH)
+        self.textbox.pack(fill=BOTH, expand=True)
 
     def click_options(self):
         if self.opt_bool:
@@ -130,16 +132,19 @@ class GUI(Frame):
             self.set_sub_options()
 
     def set_win_man_frame(self):
+        self.window_manager_font = font.Font(family='times new roman', size=18, weight='bold')
+
         self.window_manager_frame = Frame(self, highlightthickness=0)
         self.window_manager_frame.place(relx=0, rely=0, anchor="nw")
+
         self.window_close = Button(self.window_manager_frame, text='×', highlightthickness=0,
-                                   font=('times new roman', 18, 'bold'), command=self._close)
+                                   font=self.window_manager_font, command=self._close)
         self.window_close.grid(row=0, column=0)
         self.window_maximize = Button(self.window_manager_frame, text='+', highlightthickness=0,
-                                      font=('times new roman', 18, 'bold'), command=self._maximize)
+                                      font=self.window_manager_font, command=self._maximize)
         self.window_maximize.grid(row=0, column=1)
         self.window_minimize = Button(self.window_manager_frame, text='−', highlightthickness=0,
-                                      font=('times new roman', 18, 'bold'), command=self._minimize)
+                                      font=self.window_manager_font, command=self._minimize)
         self.window_minimize.grid(row=0, column=2)
 
     def _close(self):
@@ -169,7 +174,7 @@ class GUI(Frame):
         self.opt_button.grid(row=0, column=0, sticky=N + E + S)
 
     def set_sub_options(self):
-        # Create the rest of the frame
+        # Create the rest of the options frame
         self.set_opt_speed()
         self.set_opt_density()
         self.set_opt_tiers()
@@ -177,10 +182,10 @@ class GUI(Frame):
 
     def set_opt_speed(self):
         self.opt_speed_label = Label(self.opt_frame, text='Refresh Speed', bg='#aaaaaa', fg='#3d008e',
-                                     highlightthickness=0, font=('courier', 25), relief=FLAT, width=16)
+                                     highlightthickness=0, font=self.options_font, relief=FLAT, width=16)
         self.opt_speed_label.grid(row=1, column=0, sticky=W + E)
 
-        self.opt_speed = Scale(self.opt_frame, label=None, font=('courier', 25), bg='#aaaaaa', fg='#3d008e',
+        self.opt_speed = Scale(self.opt_frame, label=None, font=self.options_font, bg='#aaaaaa', fg='#3d008e',
                                from_=1, to=4, bd=0, showvalue=0, orient=HORIZONTAL, relief=FLAT, highlightthickness=0,
                                activebackground='#00ff80', troughcolor='#aaaaaa', command=self.set_speed)
         self.opt_speed.set(self.int_speed)
@@ -188,10 +193,10 @@ class GUI(Frame):
 
     def set_opt_density(self):
         self.opt_density_label = Label(self.opt_frame, text='Snow Density', bg='#333333', fg='#00d165',
-                                       highlightthickness=0, font=('courier', 25), relief=FLAT)
+                                       highlightthickness=0, font=self.options_font, relief=FLAT)
         self.opt_density_label.grid(row=3, column=0, sticky=W + E)
 
-        self.opt_density = Scale(self.opt_frame, label=None, font=('courier', 25), bg='#333333', fg='#00d165',
+        self.opt_density = Scale(self.opt_frame, label=None, font=self.options_font, bg='#333333', fg='#00d165',
                                  from_=1, to=4, bd=0, showvalue=0, orient=HORIZONTAL, relief=FLAT, highlightthickness=0,
                                  activebackground='#3d008e', troughcolor='#333333', command=self.set_density)
         self.opt_density.set(self.int_density)
@@ -199,10 +204,10 @@ class GUI(Frame):
 
     def set_opt_tiers(self):
         self.opt_tiers_label = Label(self.opt_frame, text='Tree Tiers', bg='#aaaaaa', fg='#3d008e',
-                                     highlightthickness=0, font=('courier', 25), relief=FLAT)
+                                     highlightthickness=0, font=self.options_font, relief=FLAT)
         self.opt_tiers_label.grid(row=5, column=0, sticky=W + E)
 
-        self.opt_tiers = Scale(self.opt_frame, label=None, font=('courier', 25), bg='#aaaaaa', fg='#3d008e',
+        self.opt_tiers = Scale(self.opt_frame, label=None, font=self.options_font, bg='#aaaaaa', fg='#3d008e',
                                from_=1, to=13, bd=0, showvalue=0, orient=HORIZONTAL, relief=FLAT, highlightthickness=0,
                                activebackground='#00ff80', troughcolor='#aaaaaa', command=self.set_tiers)
         self.opt_tiers.set(self.int_tiers)
@@ -210,10 +215,8 @@ class GUI(Frame):
 
     def set_opt_ornaments(self):
         self.opt_ornaments_label = Label(self.opt_frame, text='Ornaments', bg='#333333', fg='#00d165',
-                                         highlightthickness=0, font=('courier', 25), relief=FLAT)
+                                         highlightthickness=0, font=self.options_font, relief=FLAT)
         self.opt_ornaments_label.grid(row=7, column=0, sticky=W + E)
-
-        self.ornaments_bool = (False, True)[self.tree.arg_dict['ornaments']]
 
         self.set_opt_ornaments_frame()
 
@@ -223,14 +226,14 @@ class GUI(Frame):
 
         button_on = (FLAT, RIDGE)[self.ornaments_bool]
         self.opt_ornaments_on = Button(self.opt_ornaments_frame, text='On', highlightthickness=0, width=6,
-                                       font=('courier', 25), relief=button_on, bg='#333333', fg='#00d165',
+                                       font=self.options_font, relief=button_on, bg='#333333', fg='#00d165',
                                        activebackground='#333333', activeforeground='#00d165',
                                        command=lambda: self.set_ornaments(True))
         self.opt_ornaments_on.grid(row=0, column=0, sticky=N + E + W + S, ipadx=1)
 
         button_off = (RIDGE, FLAT)[self.ornaments_bool]
         self.opt_ornaments_off = Button(self.opt_ornaments_frame, text='Off', highlightthickness=0, width=6,
-                                        font=('courier', 25), relief=button_off, bg='#333333', fg='#00d165',
+                                        font=self.options_font, relief=button_off, bg='#333333', fg='#00d165',
                                         activebackground='#333333', activeforeground='#00d165',
                                         command=lambda: self.set_ornaments(False))
         self.opt_ornaments_off.grid(row=0, column=1, sticky=N + E + W + S, ipadx=1)
@@ -238,6 +241,8 @@ class GUI(Frame):
     def set_options(self):
         if self.opt_frame:
             self.opt_frame.destroy()
+        self.options_font = font.Font(family='courier', size=25)
+
         self.opt_frame = Frame(self, width=65, height=26, highlightthickness=0)
         self.opt_frame['bg'] = '#000000'
         self.opt_frame.place(relx=1, rely=0, x=-2, y=2, anchor=NE)
@@ -245,49 +250,29 @@ class GUI(Frame):
         self.opt_bool = False
 
     def set_speed(self, value):
-        if not self.busy:
-            self.busy = True
-            before = self.curr_speed
-            value = int(value)
-            args = self.tree.arg_dict
-            speeds = ['slow', 'average', 'fast', 'ultra']
-            self.int_speed = value
-            self.curr_speed = speeds[self.int_speed - 1]
-            args['speed'] = self.curr_speed
-            self.reset_tree(args)
-            print_change('Speed', before, self.curr_speed)
+        before = self.curr_speed
+        self.int_speed = int(value)
+        self.curr_speed = parameters.retrieve_speed_choices()[self.int_speed - 1]
+        self.reset_tree('speed', self.curr_speed)
+        print_change('Speed', before, self.curr_speed)
 
     def set_density(self, value):
-        value = int(value)
-        if self.int_density is not value and not self.busy:
-            self.busy = True
-            before = self.curr_density
-            args = self.tree.arg_dict
-            densities = ['thin', 'average', 'heavy', 'ultra']
-            self.int_density = value
-            self.curr_density = densities[self.int_density - 1]
-            args['density'] = self.curr_density
-            self.reset_tree(args)
-            print_change('Density', before, self.curr_density)
+        before = self.curr_density
+        self.int_density = int(value)
+        self.curr_density = parameters.retrieve_density_choices()[self.int_density - 1]
+        self.reset_tree('density', self.curr_density)
+        print_change('Density', before, self.curr_density)
 
     def set_tiers(self, value):
-        if not self.busy:
-            self.busy = True
-            before = self.int_tiers
-            value = int(value)
-            args = self.tree.arg_dict
-            self.int_tiers = value
-            args['tiers'] = self.int_tiers
-            self.reset_tree(args)
-            print_change('Tiers', before, self.int_tiers)
+        before = self.int_tiers
+        self.int_tiers = int(value)
+        self.reset_tree('tiers',  self.int_tiers)
+        print_change('Tiers', before, self.int_tiers)
 
     def set_ornaments(self, arg_bool):
-        if not self.busy and arg_bool is not self.ornaments_bool:
-            self.busy = True
+        """Does not update if the currently-activated button is clicked again"""
+        if arg_bool is not self.ornaments_bool:
             before = self.ornaments_bool
-            args = self.tree.arg_dict
-            args['ornaments'] = arg_bool
-            self.reset_tree(args)
             self.ornaments_bool = arg_bool
 
             button_on = (FLAT, RIDGE)[self.ornaments_bool]
@@ -295,24 +280,20 @@ class GUI(Frame):
             button_off = (RIDGE, FLAT)[self.ornaments_bool]
             self.opt_ornaments_off.configure(relief=button_off)
 
+            self.reset_tree('ornaments', arg_bool)
             print_change('Ornaments', before, self.ornaments_bool)
 
     def set_screen_width(self):
-        if not self.busy:
-            self.busy = True
-            before = self.tree.screen_width
-            args = self.tree.arg_dict
-            args['width'] = max(self.w_dim // 6 - 2, self.tree.tree_width + 1)
-            self.reset_tree(args)
-            print_change('Window Width', before, self.tree.screen_width)
+        before = self.tree.screen_width
+        self.reset_tree('width', max(self.w_dim // 6 - 2, self.tree.tree_width + 1))
+        print_change('Window Width', before, self.tree.screen_width)
 
-    def reset_tree(self, args):
+    def reset_tree(self, key, value):
         # self.textbox.place_forget()
         # self.set_text_box()
-        self.tree.arg_dict = args
-        self.tree.set_parameters()
-        self.print_init()
-        self.busy = False
+        self.tree.arg_dict[key] = value
+        self.tree.update_parameters()
+        self.print_trees_now()
 
     def window_change(self, event):
         # before = f'{self.w_dim}x{self.h_dim}+{self.x_dim}+{self.y_dim}'
@@ -325,22 +306,24 @@ class GUI(Frame):
         self.root.geometry('{}x{}'.format(self.w_dim, self.h_dim))
         # print_change('Dimensions', before, f'{self.w_dim}x{self.h_dim}+{self.x_dim}+{self.y_dim}')
 
-    def print_init(self):
-        """Print the initial 6 Trees to the GUI"""
+    def print_trees_now(self):
+        """Prints the Trees to the GUI"""
         initial_tree_str = ''
-        for curr_tree in range(6):
-            initial_tree_str += self.tree.list[curr_tree] + '\n'
+        for curr_tree in range((self.h_dim // 13 // self.tree.screen_height) + 1):
+            initial_tree_str += self.tree.list[curr_tree % self.tree.length] + '\n'
+            # print('printing tree #', curr_tree + 1)
         self.textbox.insert('0.0', initial_tree_str)
 
     def run_gui(self, textbox, index):
         """Recursive loop that prints the tree at the top of the GUI"""
         textbox.insert('0.0', self.tree.list[index] + '\n')
         # pause execution for the time specified in the speed argument provided.
-        textbox.after(int(self.tree.sleep_time * 1000),
-                      self.run_gui, textbox, (index + 1) % self.tree.length)
+        textbox.after(int(self.tree.sleep_time * 1000), self.run_gui,
+                      textbox, (index + 1) % self.tree.length)
 
 
 def print_change(type_of, before, after):
+    """Prints the changed option to the console with before and after values"""
     if str(before) != str(after):
         indent = (' * ', '')[type_of != 'Dimensions']
         print(f'{indent}{type_of}: {before} => {after}')
