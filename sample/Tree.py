8b20467c1799e3f46cd6aaf77a060b7fd314abe9
mas9311 <mas9311@gmail.com>
2019-03-14 01:26:23 -0600

    Simplified the method that updates how the Tree is updated. No modifiers are in __init__, but instead, calls the update_parameters() method. Now, no double code (Update anomaly in a database).

--- sample/Tree.py
+++ sample/Tree.py
@@ -1,47 +1,48 @@
 import numpy as np
 
+from sample import parameters
+
 
 class Tree:
     def __init__(self, arg_dict):
         self.arg_dict = arg_dict
-        self.speeds = {'ultra': 0.1, 'fast': 0.465000, 'average': 0.7265000, 'slow': 0.9810000}
-        self.densities = {'ultra': 733, 'heavy': 512, 'average': 128, 'thin': 36}
-        self.tree_widths = [t_width for t_width in range(13, 68, 4)]
 
         self.tree_tiers = 0
+        self.tree_width = 0
+        self.base_height = 0
+        self.base_width = 0
         self.screen_width = 0
+        self.screen_height = 0
+        self.make_even = 0
         self.sleep_time = None
         self.max_snow = None
-        self.ornaments = None
+        self.ornaments = ['⍟', 'x', '♦']  # ⍟ x ♦ ● ○ * ★ ⍟
+        self.ornament_bool = False
         self.length = 0
         self.list = []
-        self.set_parameters(True)
 
-        self.tree_width = self.tree_widths[self.tree_tiers]
-        self.make_even = (1, 0)[(self.screen_width - self.tree_width) % 2 is 0]
         self.star_char = '★'
         self.leaf_char = '❇'  # ❇ #
         self.snow_char = '*'  # * ❇
         self.base_char = '┆'  # │ ║ ┃ ┆ ┇ ┊ ┋
+        self.update_parameters()
 
-        self.screen_width = max(self.arg_dict['width'], self.tree_width + 1)
-        self.make_even = (1, 0)[(self.screen_width - self.tree_width) % 2 is 0]
-        self.build_list()
+    def update_parameters(self):
+        tree_width_choices = [t_width for t_width in range(13, 68, 4)]
 
-    def set_parameters(self, initial_build=False):
         self.tree_tiers = self.arg_dict['tiers']
-        self.screen_width = self.arg_dict['width']
-        self.sleep_time = self.speeds[self.arg_dict['speed']]
-        self.max_snow = self.densities[self.arg_dict['density']]
-        self.length = max(self.arg_dict['length'], 25)
-        self.ornaments = ([], ['⍟', 'x', '♦'])[self.arg_dict['ornaments']]
-        # Additional ornaments  ● x ♦ ○ * ★ ⍟
+        self.tree_width = tree_width_choices[self.tree_tiers]
+        self.base_width = min(1 + ((self.tree_tiers // 2) * 2), 9)
+        self.base_height = min(max(self.tree_tiers - 1, 1), 6)
+        self.screen_width = max(self.arg_dict['width'], self.tree_width)
+        self.screen_height = 1 + (1 + (6 + (4 * (self.tree_tiers - 1)) + self.base_height)) + 1
+        self.make_even = (1, 0)[(self.screen_width - self.tree_width) % 2 is 0]
+        self.sleep_time = parameters.retrieve_speed_dict()[self.arg_dict['speed']]
+        self.max_snow = parameters.retrieve_density_dict()[self.arg_dict['density']]
+        self.ornament_bool = self.arg_dict['ornaments']
+        self.length = max(self.arg_dict['length'], 5)
 
-        if not initial_build:
-            self.tree_width = self.tree_widths[self.tree_tiers]
-            self.screen_width = max(self.arg_dict['width'], self.tree_width + 1)
-            self.make_even = (1, 0)[(self.screen_width - self.tree_width) % 2 is 0]
-            self.build_list()
+        self.list = [str(self) for _ in range(self.length)]
 
     def _gen_snow(self, snow_len):
         output = ''
@@ -73,12 +74,8 @@ class Tree:
 
     def _tree_base(self):
         output = ''
-        base_width = 1 + ((self.tree_tiers // 2) * 2)
-        base_width = (base_width, 9)[base_width > 9]
-        base_height = (self.tree_tiers - 1, 1)[self.tree_tiers is 1]
-        base_height = (base_height, 6)[base_height > 6]
-        for _ in range(base_height):
-            output += self._make_base(base_width)
+        for _ in range(self.base_height):
+            output += self._make_base(self.base_width)
         return output + self._gen_snow(self.screen_width)
 
     def _star_line(self):
@@ -94,17 +91,20 @@ class Tree:
     def _gen_leaf(self, leaves):
         output = ''
         for _ in range(leaves):
-            r = int(np.random.random_sample() * 1000)
-            if 500 <= r < 550 and len(self.ornaments) >= 1:     # 5.0% chance of first ornament
-                output += self.ornaments[0]
-            elif 600 <= r < 635 and len(self.ornaments) >= 2:   # 3.5% chance of second ornament
-                output += self.ornaments[1]
-            elif 700 <= r < 725 and len(self.ornaments) >= 3:   # 2.5% chance of third ornament
-                output += self.ornaments[2]
-            elif 800 <= r < 820 and len(self.ornaments) >= 4:   # 2.0% chance of fourth ornament
-                output += self.ornaments[3]
-            elif 900 <= r < 915 and len(self.ornaments) >= 5:   # 1.5% chance of fifth ornament
-                output += self.ornaments[4]
+            if self.ornament_bool:
+                r = int(np.random.random_sample() * 1000)
+                if 500 <= r < 550 and len(self.ornaments) >= 1:     # 5.0% chance of first ornament
+                    output += self.ornaments[0]
+                elif 600 <= r < 635 and len(self.ornaments) >= 2:   # 3.5% chance of second ornament
+                    output += self.ornaments[1]
+                elif 700 <= r < 725 and len(self.ornaments) >= 3:   # 2.5% chance of third ornament
+                    output += self.ornaments[2]
+                elif 800 <= r < 820 and len(self.ornaments) >= 4:   # 2.0% chance of fourth ornament
+                    output += self.ornaments[3]
+                elif 900 <= r < 915 and len(self.ornaments) >= 5:   # 1.5% chance of fifth ornament
+                    output += self.ornaments[4]
+                else:
+                    output += self.leaf_char
             else:
                 output += self.leaf_char
         return output
@@ -114,10 +114,6 @@ class Tree:
         base_str = self.base_char * width
         spaces = (self.screen_width - self.tree_width + 2) // 2
         return self._gen_snow(spaces) + buffer + base_str + buffer + self._gen_snow(spaces + self.make_even) + '\n'
-
-    def build_list(self):
-        """Generates the list of randomly arranged snow and ornament locations to be printed."""
-        self.list = [str(self) for _ in range(self.length)]
 
     def __str__(self):
         return (f'{self._tree_topper()}'
